ae62edc9f2ec330e8bb96977c67c147d2fcc52cd
mgarton1 <mgarton1@iastate.edu>
2018-10-21 19:49:24 -0500

    Add MainActivity to hold bottom nav control

--- androidApp/app/src/main/java/com/secretsnowman/secretsnowman/RegisterActivity.java
+++ androidApp/app/src/main/java/com/secretsnowman/secretsnowman/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.secretsnowman.secretsnowman;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -24,15 +25,21 @@ public class RegisterActivity extends AppCompatActivity {
         final EditText t = findViewById(R.id.editText_register);
         helper = new DatabaseHelper(getApplicationContext());
         currentUser = helper.getUser(1);
-        b.setOnClickListener(new View.OnClickListener() {
+        final Intent startMainIntent = new Intent(this, MainActivity.class);
+        if(currentUser != null){ // If the user already registered and there is a stored instance of them, open the main app
+            startActivity(startMainIntent);
+            finish(); // Prevents the user from hitting back to get back to the register screen
+        }
+        b.setOnClickListener(new View.OnClickListener() { // If the user has not registered, stay in the register activity
             @Override
             public void onClick(View v) {
                if(currentUser == null){
                    User userToBeAdded = new User(0, t.getText().toString(), 0);
-                   Log.d("User's id:", String.valueOf(helper.insertUser(userToBeAdded)));
+                   helper.insertUser(userToBeAdded);
+                   // TODO: Create the server request to register a user and then update the local user with the serverId
+                   startActivity(startMainIntent);
+                   finish(); // Prevents the user from hitting back to get back to the register screen
                }
-               else
-                   Toast.makeText(getApplicationContext(), "Hello, " + currentUser.getName(), Toast.LENGTH_SHORT).show();
             }
         });
     }
